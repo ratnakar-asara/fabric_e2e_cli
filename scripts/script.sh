330c2d7dfa8597255a15540ddba9a5b1833b04ba
ratnakar <asara_ratnakar@yahoo.co.in>
2017-05-02 14:39:44 -0400

    Fix
    Signed-off-by: ratnakar <asara_ratnakar@yahoo.co.in>

--- scripts/script.sh
+++ scripts/script.sh
@@ -33,7 +33,7 @@ setGlobals () {
 		fi
 	else
 		CORE_PEER_LOCALMSPID="Org1MSP"
-		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org2.example.com/tls/ca.crt
+		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 		if [ $1 -eq 2 ]; then
 			CORE_PEER_ADDRESS=peer0.org2.example.com:7051
 			CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/msp				
