a07e56be5e7ca9e54c19f0eb9ec23cb705fd89f5
ratnakar-asara <asara_ratnakar@yahoo.co.in>
2017-05-09 17:21:19 -0400

    Add Org3 to the network
    Signed-off-by: ratnakar-asara <asara_ratnakar@yahoo.co.in>

--- generateArtifacts.sh
+++ generateArtifacts.sh
@@ -23,15 +23,21 @@ function replacePrivateKey () {
 
 	cp docker-compose-e2e-template.yaml docker-compose-e2e.yaml
 
-        CURRENT_DIR=$PWD
-        cd crypto-config/peerOrganizations/org1.example.com/ca/
-        PRIV_KEY=$(ls *_sk)
-        cd $CURRENT_DIR
-        sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
-        cd crypto-config/peerOrganizations/org2.example.com/ca/
-        PRIV_KEY=$(ls *_sk)
-        cd $CURRENT_DIR
-        sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+    CURRENT_DIR=$PWD
+    cd crypto-config/peerOrganizations/org1.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd $CURRENT_DIR
+    sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+
+    cd crypto-config/peerOrganizations/org2.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd $CURRENT_DIR
+    sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+
+    cd crypto-config/peerOrganizations/org3.example.com/ca/
+    PRIV_KEY=$(ls *_sk)
+    cd $CURRENT_DIR
+    sed $OPTS "s/CA3_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
 }
 
 ## Generates Org certs using cryptogen tool
@@ -77,15 +83,12 @@ function generateChannelArtifacts() {
 
 	echo
 	echo "#################################################################"
-	echo "#######    Generating anchor peer update for Org1MSP   ##########"
+	echo "### Generating anchor peer update for Org1MSP/Org2MSP/Org3MSP ###"
 	echo "#################################################################"
-	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
-
+	for orgMsp in Org1MSP Org2MSP Org3MSP ; do
+	    $CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/${orgMsp}anchors.tx -channelID $CHANNEL_NAME -asOrg $orgMsp
+    done
 	echo
-	echo "#################################################################"
-	echo "#######    Generating anchor peer update for Org2MSP   ##########"
-	echo "#################################################################"
-	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
 	echo
 }
 
